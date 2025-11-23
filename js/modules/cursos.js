494d00755201819dacdfd3013d1e9ae1b7dac1e4
GabrielkPrata <164642470+GabrielkPrata@users.noreply.github.com>
2025-11-22 21:19:11 -0300

    Update fetch and course methods to use defaultHeaders(true)

--- js/modules/cursos.js
+++ js/modules/cursos.js
@@ -2,14 +2,14 @@
 import { SUPABASE_URL, defaultHeaders } from './supabase.js';
 export async function fetchCourses() {
     const res = await fetch(`${SUPABASE_URL}/rest/v1/courses?select=*`, {
-        headers: defaultHeaders(false)
+        headers: defaultHeaders(true)
     });
     if (!res.ok) throw new Error('Falha ao buscar cursos');
     return res.json();
 }
 export async function createCourse(course) {
     const res = await fetch(`${SUPABASE_URL}/rest/v1/courses`, {
-        method: 'POST', headers: defaultHeaders(false), body:
+        method: 'POST', headers: defaultHeaders(true), body:
             JSON.stringify(course)
     });
     if (!res.ok) throw new Error('Falha ao criar curso');
@@ -17,14 +17,14 @@ export async function createCourse(course) {
 }
 export async function updateCourse(id, course) {
     const res = await fetch(`${SUPABASE_URL}/rest/v1/courses?id=eq.${id}`, {
-        method: 'PATCH', headers: defaultHeaders(false), body: // CORRIGIDO (Removido o 6)
+        method: 'PATCH', headers: defaultHeaders(true), body: // CORRIGIDO (Removido o 6)
             JSON.stringify(course)
     });
     return res.json();
 }
 export async function deleteCourse(id) {
     const res = await fetch(`${SUPABASE_URL}/rest/v1/courses?id=eq.${id}`, {
-        method: 'DELETE', headers: defaultHeaders(false)
+        method: 'DELETE', headers: defaultHeaders(true)
     });
     return res.ok;
 }
